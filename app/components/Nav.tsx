d1da00038630547696a6b3952660f8a4d7c5e9da
salted-caramel <105400878+salted-caramel@users.noreply.github.com>
2024-06-04 09:57:19 +0800

    add shortcut to admin panel

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const Nav = () => {
+  const [clickCount, setClickCount] = useState(0);
+  const router = useRouter();
+
+  const handleButtonClick = () => {
+    setClickCount((prevCount) => {
+      const newCount = prevCount + 1;
+      if (newCount === 7) {
+        router.push("/add");
+      }
+      return newCount;
+    });
+  };
+
   return (
     <div className="navbar bg-gray-800">
       <div className="navbar-start">
-        <a className="btn btn-ghost text-xl">
+        <button className="btn btn-ghost text-xl" onClick={handleButtonClick}>
           <p className="text-xl">Mr Lee</p>
-        </a>
+        </button>
       </div>
       <div className="navbar-end">{/* <a className="btn">中文</a> */}</div>
     </div>
