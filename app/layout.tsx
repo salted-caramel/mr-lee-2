2a6b0db31c76850d40155abc261f164306b4525d
salted-caramel <105400878+salted-caramel@users.noreply.github.com>
2024-06-14 08:38:05 +0800

    add google analy tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
+// layout.tsx
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Mr Lee",
-  description: "",
+  description: "TCM Chiropractor",
 };
 
 export default function RootLayout({
@@ -16,7 +18,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="bg-gray-800">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <meta name="google-adsense-account" content="ca-pub-7522199614879430" />
+        <meta name="robots" content="index, follow" />
+      </head>
+      <body className={inter.className}>
+        {children}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-BQEFECH8GR"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-BQEFECH8GR');
+            `,
+          }}
+        />
+      </body>
     </html>
   );
 }
