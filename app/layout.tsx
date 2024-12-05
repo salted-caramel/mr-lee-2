9c77df93802dd850289baea68411144d9b5d7ec3
salted-caramel <105400878+salted-caramel@users.noreply.github.com>
2024-12-05 14:02:02 +0800

    add canonical

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
           name="keywords"
           content="mr lee, tcm, chiropractor, acupuncture, boon keng, upper boon keng, ubk, mr lee ubk, 李先生, 中医, 推拿, 新加坡推拿"
         />
+        <link rel="canonical" href="https://mrlee.sg" />
       </head>
       <body className={inter.className}>
         {children}
